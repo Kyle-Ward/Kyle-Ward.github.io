89e0a36170ed9f0d7f95e90d2c7e217306c0079a
Kyle-Ward <KyleWard38@gmail.com>
2019-04-08 16:51:56 -0500

    cleaned up code and ran build

--- Andrei_Neagoie/Section_19_React_Redux/robofriends/src/containers/App.js
+++ Andrei_Neagoie/Section_19_React_Redux/robofriends/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     super();
     this.state = {
       robots: [],
-      searchfield: ""
+      searchfield: ''
     };
   }
 
@@ -24,15 +24,15 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+        .includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
+      return !robots.length ?
+      <h1>Loading...</h1> :
+      (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -42,7 +42,6 @@ class App extends Component {
         </div>
       );
     }
-  }
 }
 
 export default App;
