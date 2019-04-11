84622da45a7987587e5888468837faa6081fa1b9
Kyle-Ward <KyleWard38@gmail.com>
2019-04-11 15:02:45 -0500

    changed filenames

---
+++ andrei_neagoie/section20_http_json_ajax/promises.js
@@ -0,0 +1,44 @@
+const promise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("Stuff worked");
+  } else {
+    reject("Error, it broke");
+  }
+});
+
+promise.then(result => console.log(result)); //returns 'Stuff worked'
+
+
+//multiple .then(s) is called chaining
+promise
+  .then(result => result + '!')
+  .then(result2 => {
+    console.log(result2); //returns 'Stuff worked!'
+  });
+
+
+  promise
+  .then(result => result + '!')
+  .then(result2 => {
+    throw Error
+    console.log(result2); //returns 'Stuff worked!'
+  })
+  .catch(() => console.log('error!')); //.catch catches any errors that may happen before it is called throughout the .then chain
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'Hi!')
+  })
+
+  const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000, 'Pookie')
+  })
+
+  const promise4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 5000, "Is it me you're looking for?")
+  })
+
+  //Promise.all takes an array of promises as an argument and can return their resolved or rejected values as an array. The first time it returns it waits until all of the promise arguments are resolved before returning anything.
+  Promise.all([promise, promise2, promise3, promise4])
+    .then(values => {
+      console.log(values);
+    })
