1b025503c4e4d86e47afed8c952eebc489ed992d
Kyle-Ward <KyleWard38@gmail.com>
2019-04-16 15:18:38 -0500

    added functionality to form

--- andrei_neagoie/section23_final_project/smartbrain/src/App.js
+++ andrei_neagoie/section23_final_project/smartbrain/src/App.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
-import Navigation from './components/navigation/Navigation'
-import Logo from './components/logo/Logo'
-import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
-import Rank from './components/rank/Rank';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
+import Rank from "./components/rank/Rank";
+import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition />} */}
       </div>
     );
