0318c424ee5103da2cc93b331078fa710d3726dc
Kyle-Ward <KyleWard38@gmail.com>
2019-04-16 14:44:31 -0500

    added ranking component

--- andrei_neagoie/section23_final_project/smartbrain/src/App.js
+++ andrei_neagoie/section23_final_project/smartbrain/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import Rank from './components/rank/Rank';
 import './App.css';
 
 class App extends Component {
