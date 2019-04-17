90b975ccd4e84359f1d96bf99c08ab8294728de3
Kyle-Ward <KyleWard38@gmail.com>
2019-04-17 16:18:42 -0500

    added more routing through registration, signin, and homepage

--- andrei_neagoie/section23_final_project/smartbrain/src/components/signin/Signin.js
+++ andrei_neagoie/section23_final_project/smartbrain/src/components/signin/Signin.js
@@ -5,11 +5,11 @@ const Signin = ({ onRouteChange }) => {
     <div>
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure">
+          <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" for="email-address">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
                   Email
                 </label>
                 <input
@@ -20,7 +20,7 @@ const Signin = ({ onRouteChange }) => {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" for="password">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
                   Password
                 </label>
                 <input
@@ -40,11 +40,11 @@ const Signin = ({ onRouteChange }) => {
               />
             </div>
             <div className="lh-copy mt3">
-              <p onClick={() => onRouteChange("register")} className="f6 link dim black db pointer">
+              <p onClick={() => onRouteChange("registration")} className="f6 link dim black db pointer">
                 Register
               </p>
             </div>
-          </form>
+          </div>
         </main>
       </article>
     </div>
