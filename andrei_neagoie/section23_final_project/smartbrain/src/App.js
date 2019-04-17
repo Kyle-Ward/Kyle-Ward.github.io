fd3e3195f8f5b1dfa21925c77844a30a36d77788
Kyle-Ward <KyleWard38@gmail.com>
2019-04-17 15:27:12 -0500

    added sign in/sign out routing and form

--- andrei_neagoie/section23_final_project/smartbrain/src/App.js
+++ andrei_neagoie/section23_final_project/smartbrain/src/App.js
@@ -6,6 +6,7 @@ import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
 import Rank from "./components/rank/Rank";
 import FaceRecognition from "./components/facerecognition/FaceRecognition";
+import Signin from "./components/signin/Signin";
 import "./App.css";
 
 const app = new Clarifai.App({
@@ -30,26 +31,28 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
-      box: {}
+      box: {},
+      route: "signin"
     };
   }
 
   calculateFaceLocation = data => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
   };
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
-  }
+  displayFaceBox = box => {
+    this.setState({ box: box });
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -59,22 +62,37 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
       .catch(err => console.log(err));
   };
+
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
 
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        )}
       </div>
     );
   }
