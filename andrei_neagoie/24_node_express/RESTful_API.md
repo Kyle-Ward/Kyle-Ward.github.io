f2e25756379206bc400db197d23300c9fa0f9e98
Kyle-Ward <KyleWard38@gmail.com>
2019-04-19 12:44:09 -0500

    RESTful API

---
+++ andrei_neagoie/24_node_express/RESTful_API.md
@@ -0,0 +1,8 @@
+With RESTFUL APIs devs can define a set of functions to perform requests and receive responses (get, post, put, delete) using HTTP. They make us able to create rules that govern compatibility between front and back ends.
+
+Get - receive a resource
+Put - change/update a resource
+Post - create a resource
+delete - remove a resource
+
+RESTful APIs are stateless, meaning that each call can be made independently of one another and each call has all the data necessary to complete successfully.
--- andrei_neagoie/24_node_express/expressServer.js
+++ andrei_neagoie/24_node_express/expressServer.js
@@ -2,23 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-/*EXPRESS AUTOMATICALLY CONVERTS TYPES:*/
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("getting root");
-});
+/*EXPRESS AUTOMATICALLY CONVERTS TYPES:*/
 
-app.get("/profile", (req, res) => {
-  res.send("getting profile");
-});
+// app.get("/", (req, res) => {
+//   res.send("getting root");
+// });
 
-app.post("/profile", (req, res) => {
-  console.log(req.body);
-  res.send("Success");
-});
+// app.get("/profile", (req, res) => {
+//   res.send("getting profile");
+// });
+
+// app.post("/profile", (req, res) => {
+//   console.log(req.body);
+//   res.send("Success");
+// });
 
 
 /***********************************************/
@@ -35,5 +35,27 @@ app.post("/profile", (req, res) => {
 // app.get("/", (req, res) => {
 //   res.send("test");
 // });
+
+/*****************************************/
+
+//RESTful APIs:
+
+// app.get("/", (req, res) => {
+  //GET Restful API
+  // req.query //used to show info after ? within the url string
+            //example: /?name=kyle&age=33
+  // req.body //used to show whatever the requests sends in the body
+  // req.headers //used to show info within headers
+  // req.params //used to show paramaters of the url
+
+  //can respond with a status:
+  // res.status(404).send("not found");
+// });
+
+/**********************************************/
+
+// Use middleware to get express to send static files:
+
+// app.use(express.static(__dirname + "/public"))
 
 app.listen(3000);
