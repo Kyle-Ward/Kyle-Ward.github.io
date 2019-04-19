7456363356f81bc6ab4619db09ea5fa22c535b07
Kyle-Ward <KyleWard38@gmail.com>
2019-04-19 11:13:04 -0500

    node.js basics

---
+++ andrei_neagoie/24_node_express/script.js
@@ -0,0 +1,22 @@
+//USING MODULES IN NODE:
+
+//SELF BUILT:
+
+const script2 = require('./script2');
+
+const a = script2.largeNum;
+const b = 5;
+
+console.log(a + b);
+
+//***********************************/
+//BUILT IN WITH NODE:
+
+// fs = file system module
+// const c = require("fs");
+// console.log(c);
+
+// http = module to build server
+// const s = require("http")
+
+//
---
+++ andrei_neagoie/24_node_express/script2.js
@@ -0,0 +1,10 @@
+// USING MODULES IN NODE:
+
+// SELF BUILT:
+
+const largeNum = 356;
+module.exports ={
+  largeNum: largeNum
+}
+
+//*************************************/
