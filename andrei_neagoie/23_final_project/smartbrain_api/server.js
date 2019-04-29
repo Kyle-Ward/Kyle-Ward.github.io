682ddaf7fc16a05e91ab6238ddf70c1cd91510cc
Kyle-Ward <KyleWard38@gmail.com>
2019-04-29 13:19:55 -0500

    added database functionality for /profile/:id

--- andrei_neagoie/23_final_project/smartbrain_api/server.js
+++ andrei_neagoie/23_final_project/smartbrain_api/server.js
@@ -88,16 +88,17 @@ app.post("/register", (req, res) => {
 //PROFILE
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json("not found");
-  }
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found.");
+      }
+    })
+    .catch(err => res.status(400).json("Error getting user."));
 });
 
 //# of Images done
