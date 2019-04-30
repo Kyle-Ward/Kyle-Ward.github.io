7d43770103e789bfdbaeef7d9b432cd5d4e9117f
Kyle-Ward <KyleWard38@gmail.com>
2019-04-30 15:06:19 -0500

    changed endpoint urls to heroku server address

--- andrei_neagoie/23_final_project/smartbrain/src/components/registration/Registration.js
+++ andrei_neagoie/23_final_project/smartbrain/src/components/registration/Registration.js
@@ -23,7 +23,7 @@ class Registration extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://peaceful-reef-28329.herokuapp.com/register", {
       method: "post",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
