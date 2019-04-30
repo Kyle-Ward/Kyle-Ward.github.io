f4ee380e33725cd4af9d1860dd1f866f10a00f33
Kyle-Ward <KyleWard38@gmail.com>
2019-04-30 11:13:08 -0500

    exported server routes to controllers folder to clean up server.js

--- andrei_neagoie/23_final_project/smartbrain_api/server.js
+++ andrei_neagoie/23_final_project/smartbrain_api/server.js
@@ -4,6 +4,11 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knew = require("knex");
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const db = knew({
   client: "pg",
   connection: {
@@ -25,83 +30,22 @@ app.get("/", (req, res) => {
 
 //SIGN IN
 app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json("Unable to get user"));
-      } else {
-        res.status(400).json("Wrong credentials");
-      }
-    })
-    .catch(err => res.status(400).json("Wrong credentials."));
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 //REGISTER
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("Unable to register."));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 //PROFILE
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not found.");
-      }
-    })
-    .catch(err => res.status(400).json("Error getting user."));
+  profile.handleProfileGet(req, res, db);
 });
 
 //# of Images done
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json("Unable to get entires."));
+  image.handleImage(req, res, db);
 });
 
 app.listen(3000, () => {
